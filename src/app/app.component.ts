2324e35ec7389a35d9c1d7852824a15e4a26dcac
Marcos Santana <marcos.vssjr@outlook.com>
2025-04-30 16:26:58 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
 import { ListaLivrosComponent } from "./componentes/lista-livros/lista-livros.component";
-import { GeneroLiterarioComponent } from "./componentes/genero-literario/genero-literario.component";
 
 // aqui é onde criamos o componente principal do nosso projeto, o AppComponent
 // ele é o ponto de entrada da nossa aplicação Angular
 // responsavel por carregar os outros componentes e exibir o conteúdo na tela
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent, ListaLivrosComponent, GeneroLiterarioComponent],
+  imports: [CabecalhoComponent, RodapeComponent, ListaLivrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Livro } from './livro';
+import { CommonModule } from '@angular/common';
 // a classe ts é o arquivo de logica do componente
 // aqui é onde criamos o componente LivroComponent
 // o componente é uma classe que representa uma parte da interface do usuário
 // ele é responsável por exibir os dados e interagir com o usuário
 @Component({
   selector: 'app-livro',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './livro.component.html',
   styleUrl: './livro.component.css'
 })
